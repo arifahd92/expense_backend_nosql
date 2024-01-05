138f2dc25ddace11e4e51191f000df8cd0366dcb
md arif <arifahd92@gmail.com>
2024-01-05 16:28:54 +0530

    optemized report card

--- controllers/premium.js
+++ controllers/premium.js
@@ -1,3 +1,4 @@
+const { default: mongoose } = require('mongoose');
 const sequelize = require ('../db/connection');
 
 const Expense = require ('../models/expense');
@@ -29,95 +30,42 @@ const leaderboard = async (req, res, next) => {
 
 
 //m-get=>/premium/report-card
-/*
+
 const reportCard = async (req, res) => {
   const userId = req.userId;
 
   try {
-    // Find the user by ID
-    const user = await User.findOne ({
-      where: {
-        id: userId,
+    // Aggregate pipeline to group expenses by category and calculate total for each category
+    const result = await Expense.aggregate([
+      {
+        $match: { creator: userId },
       },
-      attributes: [
-        'name',
-        'email',
-        'totalShoppingExpense',
-        'totalRentExpense',
-        'totalMovieExpense',
-        'totalGrocceryExpense',
-        'totalExpenseAmount',
-      ],
-    });
+      {
+        $group: {
+          _id: "$category",
+          expenses: { $push: "$$ROOT" },
+          total: { $sum: { $toDouble: "$amount" } },
+        },
+      },
+      {
+        $sort: {
+          _id: 1, // Sort by category in ascending order
+        },
+      },
+    ]);
 
-    if (!user) {
-      return res.status (404).json ({message: 'User not found'});
-    }
-    const categoryArr = Object.values (user.dataValues).filter (
-      elm => elm !== 0
-    );
-    console.log (user);
-    console.log (
-      '**********************************************************************'
-    );
-    // console.log (categoryArr);
-    // Fetch the user's expenses, sorted by category in descending order
-    const expenses = await Expense.findAll ({
-      where: {UserId: userId},
-      order: [['category', 'DESC']], // Sort by category in descending order
-    });
-    let arr = [];
-    arr.push ({
-      name: categoryArr[0],
-      email: categoryArr[1],
-      total: categoryArr[categoryArr.length - 1],
-    });
-    let i = 2;
-    expenses.forEach ((elm, ind) => {
-      console.log ('=============================================');
-      if (
-        ind != expenses.length - 1 &&
-        elm.category != expenses[ind + 1].category
-      ) {
-        arr.push (elm);
-        arr.push ({[elm.category]: categoryArr[i++]});
-      } else {
-        arr.push (elm);
-      }
-      if (ind == expenses.length - 1) {
-        arr.push ({[expenses[ind - 1].category]: categoryArr[i++]});
-      }
-    });
-    res.send (arr);
-  } catch (error) {
-    console.error (error);
-    res.status (500).json ({message: 'Internal server error'});
-  }
-};
-*/
-const reportCard = async (req, res) => {
-  const userId = req.userId;
-
-  try {
-    // Find the user by ID
+    // Fetch the user details
     const user = await User.findById(userId, {
       name: 1,
       email: 1,
-      totalShoppingExpense: 1,
-      totalRentExpense: 1,
-      totalMovieExpense: 1,
-      totalGrocceryExpense: 1,
       totalExpenseAmount: 1,
     });
 
     if (!user) {
       return res.status(404).json({ message: 'User not found' });
     }
-//console.log({user})
-    // Fetch the user's expenses
-    const expenses = await Expense.find({ creator: userId })
-      .sort({ category: -1 }); // Sort by category in descending order
 
+    // Prepare the response array
     const arr = [
       {
         name: user.name,
@@ -126,19 +74,10 @@ const reportCard = async (req, res) => {
       },
     ];
 
-    expenses.forEach((expense, index) => {
-      if (
-        index !== expenses.length - 1 &&
-        expense.category !== expenses[index + 1].category
-      ) {
-        arr.push(expense, { [expense.category]: user[`total${expense.category}Expense`] });
-      } else {
-        arr.push(expense);
-      }
-
-      if (index === expenses.length - 1) {
-        arr.push({ [expenses[index - 1].category]: user[`total${expenses[index - 1].category}Expense`] });
-      }
+    // Append expenses and category totals to the response array
+    result.forEach(categoryGroup => {
+      arr.push(...categoryGroup.expenses);
+      arr.push({ [categoryGroup._id]: categoryGroup.total });
     });
 
     res.send(arr);
@@ -147,6 +86,7 @@ const reportCard = async (req, res) => {
     res.status(500).json({ message: 'Internal server error' });
   }
 };
+
 
 
 
